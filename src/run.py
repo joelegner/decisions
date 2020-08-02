c31f651cad18c089ba28e5187b51839941b19289
Joe Legner <joelegner.gmail.com>
2020-08-02 13:00:51 -0400

    Added basic logging

--- src/run.py
+++ src/run.py
@@ -1,14 +1,20 @@
 from PySide2 import QtWidgets
 import sys
 from view.mainwin import MainWin
-
-print(sys.path)
+import logging
 
 if __name__ == "__main__":
     # TODO: Wrap in try catch block
     # TODO: Send argv to the _run() function
+    logging.basicConfig(filename="decisions.log",
+                        filemode='a',
+                        format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+    logging.info("Starting Qt application")
     app = QtWidgets.QApplication([])
     window = MainWin()
     window.resize(800, 600)
+    logging.info("Show window")
     window.show()
     sys.exit(app.exec_())
--- src/view/mainwin.py
+++ src/view/mainwin.py
@@ -22,3 +22,26 @@ class MainWin(QtWidgets.QWidget):
 
     def magic(self):
         self.text.setText(random.choice(self.hello))
+
+
+class MainWin(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        import logging
+        logging.info("Randomizing the label")
+        self.text.setText(random.choice(self.hello))
