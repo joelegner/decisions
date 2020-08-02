2fc9ec001aa92f01fb84cdc3a666a990521dcb62
Joe Legner <joelegner.gmail.com>
2020-08-02 11:36:44 -0400

    Added clean and build make targets

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
 SHELL := /bin/zsh
 
-.PHONY: all
-all: run
+export PIPENV_IGNORE_VIRTUALENVS=1
 
 .PHONY: run
-run:
-	echo "Starting Decisions..."; \
-	pipenv run python src/run.py
+run: build
+	@echo "Starting Application."
+	pipenv run python build/src/run.py
+
+.PHONY: build
+build: clean
+	@echo "Starting build."
+	cp -r src build
+	@echo "Build complete."
+
+.PHONY: clean
+clean:
+	@echo "Cleaning build directory."
+	rm -Rf build/*
--- src/run.py
+++ src/run.py
@@ -1,14 +1,14 @@
+from PySide2 import QtWidgets
+import sys
+from view.mainwin import MainWin
 
-# class DecisionsQtApp(QApplication):
-#    pass
-
-
-def _run():
-    pass
-
+print(sys.path)
 
 if __name__ == "__main__":
     # TODO: Wrap in try catch block
     # TODO: Send argv to the _run() function
-    print("Trying to run...")
-    _run()
+    app = QtWidgets.QApplication([])
+    window = MainWin()
+    window.resize(800, 600)
+    window.show()
+    sys.exit(app.exec_())
